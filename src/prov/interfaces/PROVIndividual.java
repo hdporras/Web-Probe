3e5c0f84dda2d7b1b1be1dec0331419a528f85a1
Hugo Porras <hdporras@miners.utep.edu>
2013-06-03 18:45:53 -0600

    OWL PROV Test

--- src/prov/interfaces/PROVIndividual.java
+++ src/prov/interfaces/PROVIndividual.java
@@ -1,21 +1,31 @@
 package prov.interfaces;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.semanticweb.owlapi.model.OWLIndividual;
+import org.semanticweb.owlapi.model.OWLObjectPropertyExpression;
+
 public class PROVIndividual
 {
 	private static int lastID = 0;
 	
-	//Number ID (for graph)
+	/** Number ID (for graph)*/
 	int id;
 
-	//Individual URI
+	/** Individual URI */
 	String uri;
 	
-	//Individual Type
+	/** Individual Type */
 	String type;
 	
-	//Individual's annotation connections.
-	String[] connections;
-	String[] connectionIDs;
+	/** Individual's Object and Data Property Connections.*/
+	String[] OPconnections;
+	String[] OPconnectionObjectIDs;
+	String[] DPconnections;
+	String[] DPconnectionObjectIDs;
 	
 	
 	public PROVIndividual(String uri)
@@ -29,5 +39,30 @@ public class PROVIndividual
 		id = lastID++;
 		this.uri = uri;
 		this.type = type;
+	}
+	
+	//*not checking for duplicates yet
+	public void addOPConnections(Map<OWLObjectPropertyExpression,Set<OWLIndividual>> map)
+	{
+		OPconnections = new String[map.size()];
+		OPconnectionObjectIDs = new String[map.size()];
+
+		Iterator<Entry<OWLObjectPropertyExpression, Set<OWLIndividual>>> connectionsIterator = map.entrySet().iterator();
+		
+		while(connectionsIterator.hasNext())
+		{
+			Entry<OWLObjectPropertyExpression, Set<OWLIndividual>> entry = connectionsIterator.next();
+			
+			String connectionPredicateName = entry.getKey().getNamedProperty().toStringID();
+			
+			Set<OWLIndividual> connectionObjects = entry.getValue();
+			//foreach connection object
+			for(OWLIndividual objectIndividual : connectionObjects)
+			{
+				String objectIndividualName = objectIndividual.toStringID();
+				
+				System.out.println(uri+"("+id+") : "+connectionPredicateName+" -> "+objectIndividualName);
+			}
+		}
 	}
 }
--- src/prov/interfaces/PROVPrimerTest.java
+++ src/prov/interfaces/PROVPrimerTest.java
@@ -3,6 +3,7 @@ package prov.interfaces;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
@@ -11,10 +12,13 @@ import org.semanticweb.owlapi.util.SimpleIRIMapper;
 
 public class PROVPrimerTest
 {
+	/** Hash map for all the nodes (URI -> number in graph) */
+	HashMap<String, PROVIndividual> individualsHM = new HashMap<String, PROVIndividual>();
+	
 
 	public String getGraph(String URI)
 	{
-		String uri = "http://trust.utep.edu/web-probe/primer-turtle-examples.ttl"; //"http://www.w3.org/TR/prov-primer/primer-turtle-examples.ttl";
+		//String uri = "http://trust.utep.edu/web-probe/primer-turtle-examples.ttl"; //"http://www.w3.org/TR/prov-primer/primer-turtle-examples.ttl";
 
 
 		//build graph from URI
@@ -22,36 +26,20 @@ public class PROVPrimerTest
 		{
 			OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
 			
-			// Let's load an ontology from the web
-			IRI iri = IRI.create(uri);//"http://www.co-ode.org/ontologies/pizza/pizza.owl");
+			// Load PROV
+			IRI iri = IRI.create(URI);//"http://www.co-ode.org/ontologies/pizza/pizza.owl");
 			OWLOntology primerExampleOntology = manager.loadOntologyFromOntologyDocument(iri);
 			System.out.println("Loaded ontology: " + primerExampleOntology);
 			
 			OWLDataFactory factory = manager.getOWLDataFactory();
 			
-			//get individuals of type prov:Activity.
-			OWLClass provActivity = factory.getOWLClass( IRI.create("http://www.w3.org/ns/prov#Activity") );
-			Set<OWLIndividual> activityIndividuals = provActivity.getIndividuals(primerExampleOntology);
+			//Activities
+			//Get individuals of type prov:Activity.
+			OWLClass provActivityClass = factory.getOWLClass( IRI.create("http://www.w3.org/ns/prov#Activity") );
+			Set<OWLIndividual> activityIndividuals = provActivityClass.getIndividuals(primerExampleOntology);
 			
-			Iterator<OWLIndividual> activityIterator = activityIndividuals.iterator();
-			
-			//Hash map for all the nodes (URI -> number in graph)
-			HashMap<String, PROVIndividual> nodesHM = new HashMap<String, PROVIndividual>();
-			
-			int activityCntr = 1;
-			while(activityIterator.hasNext())
-			{
-				OWLIndividual ind = activityIterator.next();
-				
-				PROVIndividual provInd = new PROVIndividual(ind.toStringID(), "Activity");
-				
-				//*add connections
-				
-				nodesHM.put(ind.toStringID(), provInd);
-				
-				System.out.println(activityCntr+" : "+ provInd.id +", "+ provInd.type +", "+ provInd.uri );
-				activityCntr++;
-			}
+			//Get HashMap of activities
+			getActivities(activityIndividuals, primerExampleOntology);
 			
 			
 		} 
@@ -62,28 +50,44 @@ public class PROVPrimerTest
 		//convert Java object of Graph to JSON
 		return "Done"; //convertToJSON();
 	}
+	
+	/** Get HashMap of activities (Key=URI, value = PROVIndividual) */
+	public void getActivities(Set<OWLIndividual> activityIndividuals, OWLOntology ontology)
+	{
+		
+		Iterator<OWLIndividual> activityIterator = activityIndividuals.iterator();
+		
+		int activityCntr = 1;
+		while(activityIterator.hasNext())
+		{
+			OWLIndividual ind = activityIterator.next();
+			
+			PROVIndividual provInd = new PROVIndividual(ind.toStringID(), "Activity");
+			
+			//*add connections
+			Map<OWLObjectPropertyExpression,Set<OWLIndividual>> OPvaluesMap = ind.getObjectPropertyValues(ontology);
+			Map<OWLDataPropertyExpression,Set<OWLLiteral>> DPvaluesMap = ind.getDataPropertyValues(ontology);
+			
+			provInd.addOPConnections(OPvaluesMap);
+			//provInd.addDPConnections(DPvaluesMap);
+			
+			individualsHM.put(ind.toStringID(), provInd);
+			
+			System.out.println(activityCntr+" : "+ provInd.id +", "+ provInd.type +", "+ provInd.uri );
+			activityCntr++;
+		}
+	}
 
 	
 	/** Converts the object into a JSON String representation of the object. */
 	public String convertToJSON()
 	{
+		//*
 		String json = "\"nodes\":[" +
 						   " {\"name\":\"Myriel\",\"group\":1}," +
 				           " {\"name\":\"Napoleon\",\"group\":1}," +
 				           " {\"name\":\"Mlle.Baptistine\",\"group\":1}," +
 				           " {\"name\":\"Mme.Magloire\",\"group\":1}," +
-				           " {\"name\":\"CountessdeLo\",\"group\":1}," +
-				           " {\"name\":\"Geborand\",\"group\":1}," +
-				           " {\"name\":\"Champtercier\",\"group\":1}," +
-				           " {\"name\":\"Cravatte\",\"group\":1}," +
-				           " {\"name\":\"Count\",\"group\":1}," +
-				           " {\"name\":\"Joly\",\"group\":8}," +
-				           " {\"name\":\"Grantaire\",\"group\":8}," +
-				           " {\"name\":\"MotherPlutarch\",\"group\":9}," +
-				           " {\"name\":\"Gueulemer\",\"group\":4}," +
-				           " {\"name\":\"Babet\",\"group\":4}," +
-				           " {\"name\":\"Claquesous\",\"group\":4}," +
-				           " {\"name\":\"Montparnasse\",\"group\":4}," +
 				           " {\"name\":\"Toussaint\",\"group\":5}," +
 				           " {\"name\":\"Child1\",\"group\":10}," +
 				           " {\"name\":\"Child2\",\"group\":10}," +
@@ -93,16 +97,6 @@ public class PROVPrimerTest
 				           " {\"source\":1,\"target\":0,\"value\":1}," +
 				           " {\"source\":2,\"target\":0,\"value\":8}," +
 				           " {\"source\":3,\"target\":0,\"value\":10}," +
-				           " {\"source\":3,\"target\":2,\"value\":6}," +
-				           " {\"source\":4,\"target\":0,\"value\":1}," +
-				           " {\"source\":5,\"target\":0,\"value\":1}," +
-				           " {\"source\":6,\"target\":0,\"value\":1}," +
-				           " {\"source\":7,\"target\":0,\"value\":1}," +
-				           " {\"source\":8,\"target\":0,\"value\":2}," +
-				           " {\"source\":9,\"target\":0,\"value\":1}," +
-				           " {\"source\":11,\"target\":10,\"value\":1}," +
-				           " {\"source\":11,\"target\":3,\"value\":3}," +
-				           " {\"source\":11,\"target\":2,\"value\":3}," +
 				           " {\"source\":11,\"target\":0,\"value\":5}," +
 				           " {\"source\":76,\"target\":58,\"value\":1} ]";
 		
