54b365610e53c69346df27db33cc10f3f94608ee
Hugo Porras <hdporras@miners.utep.edu>
2013-06-13 12:23:38 -0600

    Pellet Reasoner Test

--- src/prov/interfaces/PROVIndividual.java
+++ src/prov/interfaces/PROVIndividual.java
@@ -1,14 +1,23 @@
 package prov.interfaces;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLDataProperty;
 import org.semanticweb.owlapi.model.OWLDataPropertyExpression;
 import org.semanticweb.owlapi.model.OWLIndividual;
 import org.semanticweb.owlapi.model.OWLLiteral;
+import org.semanticweb.owlapi.model.OWLNamedIndividual;
+import org.semanticweb.owlapi.model.OWLObjectProperty;
 import org.semanticweb.owlapi.model.OWLObjectPropertyExpression;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.reasoner.Node;
+import org.semanticweb.owlapi.reasoner.NodeSet;
 
 public class PROVIndividual
 {
@@ -21,32 +30,127 @@ public class PROVIndividual
 	String uri;
 	
 	/** Individual Type */
-	String[] type;
+	OWLClass[] types;
 	
 	/** Individual's Object and Data Property Connections.*/
-	String[] OPconnections;
-	String[] OPconnectionObjectIDs;
-	String[] DPconnections;
-	String[] DPconnectionObjectIDs;
+	ArrayList<String> OPconnections;
+	ArrayList<Integer> OPconnectionObjectIDs;
+	
+	ArrayList<String> DPconnections;
+	ArrayList<String> DPconnectionValues;
 	
 	
 	public PROVIndividual(String uri)
 	{
 		id = lastID++;
 		this.uri = uri;
+		
+		OPconnections = new ArrayList<String>();
+		OPconnectionObjectIDs = new ArrayList<Integer>();
+		
+		DPconnections = new ArrayList<String>();
+		DPconnectionValues = new ArrayList<String>();
 	}
 	
-	public PROVIndividual(String uri, String type)
+	/*public PROVIndividual(String uri, String type)
 	{
 		id = lastID++;
 		this.uri = uri;
 		
 		this.type = new String[1];
 		this.type[0] = type;
+	}*/
+	
+	public void setTypes(OWLClass[] types)
+	{
+		this.types = types;
+	}
+	
+	
+	/** Gather information about all Activities. Indivisduals are placed in a HashMap (individualsHM: Key=URI, value = PROVIndividual) */
+	public void addActivity(OWLNamedIndividual ind)
+	{
+
+		//Get list of Object Property values associated with ind.
+		ArrayList< NodeSet<OWLNamedIndividual> > OPNodeSets = getAllActivityOPs(ind);
+		
+		//Get list of Data Property values associated with ind.
+		ArrayList< OWLLiteral > DPNodeSets = getAllActivityDPs(ind);
+		
+		//get Types
+		NodeSet<OWLClass> types = PROVPrimerTest.reasoner.getTypes( ind, true );		    
+		OWLClass[] typesArray = types.getFlattened().toArray(new OWLClass[0]);
+		setTypes(typesArray);
+		
+	}
+	
+	public ArrayList< OWLLiteral > getAllActivityDPs(OWLNamedIndividual ind)
+	{
+		ArrayList< OWLLiteral > DPNodeSets = new ArrayList< OWLLiteral >();
+		
+		//endedAtTime
+		OWLDataProperty endedAtTime = PROVPrimerTest.manager.getOWLDataFactory().getOWLDataProperty(IRI.create(PROVPrimerTest.provPrefix+"endedAtTime"));
+		Set<OWLLiteral> endedAtTimeLiterals = PROVPrimerTest.reasoner.getDataPropertyValues( ind, endedAtTime );
+		DPNodeSets.addAll(endedAtTimeLiterals);
+		//*endedAtTimeLiterals.size();
+		
+	    
+		//endedAtTime
+		OWLDataProperty startedAtTime = PROVPrimerTest.manager.getOWLDataFactory().getOWLDataProperty(IRI.create(PROVPrimerTest.provPrefix+"startedAtTime"));
+		Set<OWLLiteral> startedAtTimeLiterals = PROVPrimerTest.reasoner.getDataPropertyValues( ind, startedAtTime );
+		DPNodeSets.addAll(startedAtTimeLiterals);
+		
+		
+		return DPNodeSets;
+	}
+	
+	public ArrayList< NodeSet<OWLNamedIndividual> > getAllActivityOPs(OWLNamedIndividual ind)
+	{
+		//Where all Object Property values are contained (for this individual)
+		ArrayList< NodeSet<OWLNamedIndividual> > OPNodeSets = new ArrayList< NodeSet<OWLNamedIndividual> >();
+		
+		//*Better way of getting all object/data properties associated with the individual.
+		
+		//generated
+		OWLObjectProperty generated = PROVPrimerTest.manager.getOWLDataFactory().getOWLObjectProperty(IRI.create(PROVPrimerTest.provPrefix+"generated"));
+		NodeSet<OWLNamedIndividual> generatedObjects = PROVPrimerTest.reasoner.getObjectPropertyValues( ind, generated );
+		OPNodeSets.add(generatedObjects);
+		
+		//used
+		OWLObjectProperty used = PROVPrimerTest.manager.getOWLDataFactory().getOWLObjectProperty(IRI.create(PROVPrimerTest.provPrefix+"used"));
+		NodeSet<OWLNamedIndividual> usedObjects = PROVPrimerTest.reasoner.getObjectPropertyValues( ind, used );
+		OPNodeSets.add(usedObjects);
+		
+		//wasAssociatedWith
+		OWLObjectProperty associatedWith = PROVPrimerTest.manager.getOWLDataFactory().getOWLObjectProperty(IRI.create(PROVPrimerTest.provPrefix+"wasAssociatedWith"));
+		NodeSet<OWLNamedIndividual> associatedWithObjects = PROVPrimerTest.reasoner.getObjectPropertyValues( ind, associatedWith );
+		OPNodeSets.add(associatedWithObjects);
+		
+		//qualifiedUsage 
+		OWLObjectProperty qualifiedUsage = PROVPrimerTest.manager.getOWLDataFactory().getOWLObjectProperty(IRI.create(PROVPrimerTest.provPrefix+"qualifiedUsage"));
+		NodeSet<OWLNamedIndividual> qualifiedUsageObjects = PROVPrimerTest.reasoner.getObjectPropertyValues( ind, qualifiedUsage );
+		OPNodeSets.add(qualifiedUsageObjects);
+		
+		//qualifiedAssociation
+		OWLObjectProperty qualifiedAssociation = PROVPrimerTest.manager.getOWLDataFactory().getOWLObjectProperty(IRI.create(PROVPrimerTest.provPrefix+"qualifiedAssociation"));
+		NodeSet<OWLNamedIndividual> qualifiedAssociationObjects = PROVPrimerTest.reasoner.getObjectPropertyValues( ind, qualifiedAssociation );
+		OPNodeSets.add(qualifiedAssociationObjects);
+		
+		//wasInformedBy
+		OWLObjectProperty wasInformedBy = PROVPrimerTest.manager.getOWLDataFactory().getOWLObjectProperty(IRI.create(PROVPrimerTest.provPrefix+"wasInformedBy"));
+		NodeSet<OWLNamedIndividual> wasInformedByObjects = PROVPrimerTest.reasoner.getObjectPropertyValues( ind, wasInformedBy );
+		OPNodeSets.add(wasInformedByObjects);
+		
+		//wasInfluencedBy
+		OWLObjectProperty wasInfluencedBy = PROVPrimerTest.manager.getOWLDataFactory().getOWLObjectProperty(IRI.create(PROVPrimerTest.provPrefix+"wasInfluencedBy"));
+		NodeSet<OWLNamedIndividual> wasInfluencedByObjects = PROVPrimerTest.reasoner.getObjectPropertyValues( ind, wasInfluencedBy );
+		OPNodeSets.add(wasInfluencedByObjects);
+		
+		return OPNodeSets;
 	}
 	
 	//*not checking for duplicates yet
-	public void addOPConnections(Map<OWLObjectPropertyExpression,Set<OWLIndividual>> map)
+	public void addOPConnections()
 	{
 		OPconnections = new String[map.size()];
 		OPconnectionObjectIDs = new String[map.size()];
--- src/prov/interfaces/PROVPrimerTest.java
+++ src/prov/interfaces/PROVPrimerTest.java
@@ -1,6 +1,7 @@
 package prov.interfaces;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -19,9 +20,16 @@ import com.clarkparsia.pellet.owlapiv3.PelletReasonerFactory;
 public class PROVPrimerTest
 {
 	/** Hash map for all the nodes (URI -> number in graph) */
-	HashMap<String, PROVIndividual> individualsHM = new HashMap<String, PROVIndividual>();
+	static HashMap<String, PROVIndividual> individualsHM = new HashMap<String, PROVIndividual>();
 	
-
+	static OWLOntologyManager manager;
+	static PelletReasoner reasoner;
+	
+	static String provPrefix = "http://www.w3.org/ns/prov#";
+	
+	static int individualCntr = 1;
+	
+	
 	public String getGraph(String URI)
 	{
 		//String uri = "http://trust.utep.edu/web-probe/primer-turtle-examples.ttl"; //"http://www.w3.org/TR/prov-primer/primer-turtle-examples.ttl";
@@ -30,12 +38,12 @@ public class PROVPrimerTest
 		//build graph from URI
 		try
 		{
-			OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+			manager = OWLManager.createOWLOntologyManager();
 			
 			// Load PROV
 			IRI iri = IRI.create(URI);//"http://www.co-ode.org/ontologies/pizza/pizza.owl");
-			OWLOntology primerExampleOntology = manager.loadOntologyFromOntologyDocument(iri);
-			System.out.println("Loaded ontology: " + primerExampleOntology);
+			OWLOntology ontology = manager.loadOntologyFromOntologyDocument(iri);
+			System.out.println("Loaded ontology: " + ontology);
 			
 			//OWLOntology prov = manager.loadOntologyFromOntologyDocument(IRI.create("http://www.w3.org/ns/prov-o"));
 			
@@ -43,60 +51,21 @@ public class PROVPrimerTest
 			//Pellet Reasoner
 			System.out.println("----- Reasoner -----");
 			
-			PelletReasoner reasoner = PelletReasonerFactory.getInstance().createReasoner( primerExampleOntology );
+			reasoner = PelletReasonerFactory.getInstance().createReasoner( ontology );
 			reasoner.getKB().realize();
 			reasoner.getKB().printClassTree();
 			
-			OWLClass provActivityClass = manager.getOWLDataFactory().getOWLClass( IRI.create("http://www.w3.org/ns/prov#Activity") );
+		
+			//Activity
+			OWLClass provActivityClass = manager.getOWLDataFactory().getOWLClass( IRI.create(provPrefix+"Activity") );
 			
 			//get Individuals of Activity class.
-			NodeSet<OWLNamedIndividual> individuals = reasoner.getInstances( provActivityClass , false);
+			NodeSet<OWLNamedIndividual> activityIndividuals = reasoner.getInstances( provActivityClass , false);
+			recordActivities(activityIndividuals, ontology);
 			
-			for(Node<OWLNamedIndividual> sameInd : individuals) 
-			{
-				OWLNamedIndividual ind = sameInd.getRepresentativeElement();
-				Set<OWLLiteral> names = reasoner.getDataPropertyValues( ind, foafName );	    		    		    	
-			    NodeSet<OWLClass> types = reasoner.getTypes( ind, true );		    
-			    NodeSet<OWLNamedIndividual> homepages = reasoner.getObjectPropertyValues( ind, workHomepage );
-				
-				System.out.println( ind.toStringID() );
-			}
+			
 			
 			System.out.println("----- End of Reasoner Example -----");
-			
-			/*
-			System.out.println("----- Activities -----");
-			//Activities
-			//Get individuals of type prov:Activity.
-			OWLClass provActivityClass = factory.getOWLClass( IRI.create("http://www.w3.org/ns/prov#Activity") );
-			Set<OWLIndividual> activityIndividuals = provActivityClass.getIndividuals(merged);//primerExampleOntology);
-			
-			//Get HashMap of activities
-			getActivities(activityIndividuals, primerExampleOntology);
-			
-			
-			System.out.println("----- Agents -----");
-			//Agents
-			//Get individuals of type prov:Agent.
-			OWLClass provAgentClass = factory.getOWLClass( IRI.create("http://www.w3.org/ns/prov#Agent") );
-			Set<OWLIndividual> agentIndividuals = provAgentClass.getIndividuals(primerExampleOntology);
-			
-			//Get HashMap of activities
-			getAgents(agentIndividuals, primerExampleOntology);
-			
-			
-			
-			System.out.println("----- Enitities -----");
-			//Entities
-			//Get individuals of type prov:Entity.
-			OWLClass provEntityClass = factory.getOWLClass( IRI.create("http://www.w3.org/ns/prov#Entity") );
-			Set<OWLIndividual> entityIndividuals = provEntityClass.getIndividuals(primerExampleOntology);
-			
-			//Get HashMap of activities
-			getEntities(entityIndividuals, primerExampleOntology);
-			
-			*/
-			
 		} 
 		catch (OWLOntologyCreationException e) {
 			e.printStackTrace();
@@ -106,125 +75,27 @@ public class PROVPrimerTest
 		return "Done"; //convertToJSON();
 	}
 	
-	/** Get HashMap of activities (Key=URI, value = PROVIndividual) */
-	public void getActivitiesTest(Set<OWLIndividual> activityIndividuals, OWLOntology ontology)
+	/** Gather information about all Activities. Indivisduals are placed in a HashMap (individualsHM: Key=URI, value = PROVIndividual) */
+	public void recordActivities(NodeSet<OWLNamedIndividual> individuals, OWLOntology ontology)
 	{
 		
-		Iterator<OWLIndividual> activityIterator = activityIndividuals.iterator();
-		
-		int activityCntr = 1;
-		while(activityIterator.hasNext())
+		for(Node<OWLNamedIndividual> sameInd : individuals) 
 		{
-			OWLIndividual ind = activityIterator.next();
+			//Rep Individual
+			OWLNamedIndividual ind = sameInd.getRepresentativeElement();
+			PROVIndividual provInd = new PROVIndividual(ind.toStringID());
 			
-			PROVIndividual provInd = new PROVIndividual(ind.toStringID(), "Activity");
+			PROVPrimerTest.individualsHM.put(ind.toStringID(), provInd);
 			
-			//*add connections
-			java.util.Set<OWLObjectPropertyAssertionAxiom> OPaxioms = ontology.getObjectPropertyAssertionAxioms(ind);
-			
-			System.out.println("Number of OPs: "+OPaxioms.size());
+			provInd.addActivity(ind);
 			
 			
-			individualsHM.put(ind.toStringID(), provInd);
-			
-			System.out.println(activityCntr+" : "+ provInd.id +", "+ provInd.type +", "+ provInd.uri );
-			activityCntr++;
+			System.out.println(individualCntr+" : "+ provInd.id +", "+ provInd.types.toString() +", "+ provInd.uri );
+			individualCntr++;
 		}
+		
 	}
 	
-	
-	
-	/** Get HashMap of activities (Key=URI, value = PROVIndividual) */
-	public void getActivities(Set<OWLIndividual> activityIndividuals, OWLOntology ontology)
-	{
-		
-		Iterator<OWLIndividual> activityIterator = activityIndividuals.iterator();
-		
-		int activityCntr = 1;
-		while(activityIterator.hasNext())
-		{
-			OWLIndividual ind = activityIterator.next();
-			
-			PROVIndividual provInd = new PROVIndividual(ind.toStringID(), "Activity");
-			
-			//*add connections
-			Map<OWLObjectPropertyExpression,Set<OWLIndividual>> OPvaluesMap = ind.getObjectPropertyValues(ontology);
-			Map<OWLDataPropertyExpression,Set<OWLLiteral>> DPvaluesMap = ind.getDataPropertyValues(ontology);
-			
-			System.out.println("Object Property Map size: "+OPvaluesMap.size());
-			System.out.println("Data Property Map size: "+DPvaluesMap.size());
-			
-			provInd.addOPConnections(OPvaluesMap);
-			provInd.addDPConnections(DPvaluesMap);
-			
-			individualsHM.put(ind.toStringID(), provInd);
-			
-			System.out.println(activityCntr+" : "+ provInd.id +", "+ provInd.type +", "+ provInd.uri );
-			activityCntr++;
-		}
-	}
-	
-	
-	/** Get HashMap of activities (Key=URI, value = PROVIndividual) */
-	public void getAgents(Set<OWLIndividual> agentIndividuals, OWLOntology ontology)
-	{
-		
-		Iterator<OWLIndividual> agentIterator = agentIndividuals.iterator();
-		
-		int agentCntr = 1;
-		while(agentIterator.hasNext())
-		{
-			OWLIndividual ind = agentIterator.next();
-			
-			PROVIndividual provInd = new PROVIndividual(ind.toStringID(), "Agent");
-			
-			//*add connections
-			Map<OWLObjectPropertyExpression,Set<OWLIndividual>> OPvaluesMap = ind.getObjectPropertyValues(ontology);
-			Map<OWLDataPropertyExpression,Set<OWLLiteral>> DPvaluesMap = ind.getDataPropertyValues(ontology);
-			
-			System.out.println("Object Property Map size: "+OPvaluesMap.size());
-			System.out.println("Data Property Map size: "+DPvaluesMap.size());
-			
-			provInd.addOPConnections(OPvaluesMap);
-			provInd.addDPConnections(DPvaluesMap);
-			
-			individualsHM.put(ind.toStringID(), provInd);
-			
-			System.out.println(agentCntr+" : "+ provInd.id +", "+ provInd.type +", "+ provInd.uri );
-			agentCntr++;
-		}
-	}
-	
-	
-	/** Get HashMap of activities (Key=URI, value = PROVIndividual) */
-	public void getEntities(Set<OWLIndividual> entityIndividuals, OWLOntology ontology)
-	{
-		
-		Iterator<OWLIndividual> entityIterator = entityIndividuals.iterator();
-		
-		int entityCntr = 1;
-		while(entityIterator.hasNext())
-		{
-			OWLIndividual ind = entityIterator.next();
-			
-			PROVIndividual provInd = new PROVIndividual(ind.toStringID(), "Entity");
-			
-			//*add connections
-			Map<OWLObjectPropertyExpression,Set<OWLIndividual>> OPvaluesMap = ind.getObjectPropertyValues(ontology);
-			Map<OWLDataPropertyExpression,Set<OWLLiteral>> DPvaluesMap = ind.getDataPropertyValues(ontology);
-			
-			System.out.println("Object Property Map size: "+OPvaluesMap.size());
-			System.out.println("Data Property Map size: "+DPvaluesMap.size());
-			
-			provInd.addOPConnections(OPvaluesMap);
-			provInd.addDPConnections(DPvaluesMap);
-			
-			individualsHM.put(ind.toStringID(), provInd);
-			
-			System.out.println(entityCntr+": "+ provInd.id +"(ID), "+ provInd.type +", "+ provInd.uri );
-			entityCntr++;
-		}
-	}
 	
 	
 	public static void main(String args[])
