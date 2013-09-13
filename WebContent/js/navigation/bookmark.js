3b9758c6573992e0552a0b0590ed1087636e93d2
Hugo Porras <hdporras@miners.utep.edu>
2013-09-13 12:44:10 -0600

    CyberShare License Added
    Added the CyberShare License and Acknowledgement

--- WebContent/js/navigation/bookmark.js
+++ WebContent/js/navigation/bookmark.js
@@ -1,3 +1,17 @@
+/**
+ * @author Hugo Porras
+ * 
+ * Acknowledgements:
+ * This work used resources from Cyber-ShARE Center of Excellence, 
+ * which is supported by National Science Foundation grant number 
+ * HRD-0734825. Unless otherwise stated, work by Cyber-ShARE is 
+ * licensed under a Creative Commons Attribution 3.0 Unported 
+ * License. Permissions beyond the scope of this license may be 
+ * available at 
+ * http://cybershare.utep.edu/content/cyber-share-acknowledgment.
+ */
+
+
 /* To Add/Remove Bookmarks edit the index.html file. There is a div of id="bookmarks" in this file.
  * Follow the examples already used here to add bookmarks.
  */
--- src/prov/interfaces/PROVGraph.java
+++ src/prov/interfaces/PROVGraph.java
@@ -1,3 +1,16 @@
+/**
+ * @author Hugo Porras
+ * 
+ * Acknowledgements:
+ * This work used resources from Cyber-ShARE Center of Excellence, 
+ * which is supported by National Science Foundation grant number 
+ * HRD-0734825. Unless otherwise stated, work by Cyber-ShARE is 
+ * licensed under a Creative Commons Attribution 3.0 Unported 
+ * License. Permissions beyond the scope of this license may be 
+ * available at 
+ * http://cybershare.utep.edu/content/cyber-share-acknowledgment.
+ */
+
 package prov.interfaces;
 
 import java.io.File;
